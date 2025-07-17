22b4b459eb665a716ccc8ead8a85d19fc6ae3cda
Kittipong <kittipongtoy@hotmail.com>
2025-07-17 13:10:21 +0700

    ...

--- LoginAdmin.cs
+++ LoginAdmin.cs
@@ -47,11 +47,11 @@ namespace TOAMediaPlayer
                 if (user.ToLower() == "admin" && pass.ToLower() == "toa12345")
                 {
                     this.Close();
-                    SetupOutput xForm = new SetupOutput(player);
-                    xForm.StartPosition = FormStartPosition.CenterParent;
-                    xForm.ShowDialog();
+                    //SetupOutput xForm = new SetupOutput(player);
+                    //xForm.StartPosition = FormStartPosition.CenterParent;
+                    //xForm.ShowDialog();
 
-                    //ShowSetting();
+                    ShowSetting();
                 }
                 else
                 {
